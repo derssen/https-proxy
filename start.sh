535db1519f3fa94402ddfd081fc4d211308cf693
root <root@vm3472791.52ssd.had.wf>
2023-03-02 11:58:15 +0000

    Initial commit

---
+++ start.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "=================================================="
+if [ ! $domain ]; then
+        read -p "Enter link to domain name: " domain
+        echo 'export domain='${domain} >> $HOME/.bash_profile
+fi
+curl $domain
+echo "=================================================="
+echo "Domain name configured"
+echo "=================================================="
+curl -Lo /usr/local/bin/dumbproxy 'https://github.com/Snawoot/dumbproxy/releases/download/v1.6.1/dumbproxy.linux-amd64' && chmod +x /usr/local/bin/dumbproxy
+echo "=================================================="
+echo "App downloaded"
+dumbproxy -passwd /etc/dumbproxy.htpasswd necobra Sanya#777!
+echo 'OPTIONS=-auth basicfile://?path=/etc/dumbproxy.htpasswd -autocert -bind-address :443' >> /etc/default/dumbproxy
+echo "App configured"
+echo "[Unit]
+Description=Dumb Proxy
+Documentation=https://github.com/Snawoot/dumbproxy/
+After=network.target network-online.target
+Requires=network-online.target
+
+[Service]
+EnvironmentFile=/etc/default/dumbproxy
+User=root
+Group=root
+ExecStart=/usr/local/bin/dumbproxy $OPTIONS
+TimeoutStopSec=5s
+PrivateTmp=true
+ProtectSystem=full
+LimitNOFILE=20000
+
+[Install]
+WantedBy=default.target
+" > /etc/systemd/system/dumbproxy.service
+
+
+systemctl daemon-reload
+systemctl start dumbproxy
+
+echo "Service created"
